ae5eda5197420933791560daed130087921461aa
AhmedYounis20 <ahmed.m.younis20@gmail.com>
2025-10-23 14:47:58 +0300

    update  backend deployment url

--- src/Utilities/SD.ts
+++ src/Utilities/SD.ts
@@ -1,5 +1,5 @@
-const baseUrl = "https://aaa-erp.somee.com/api/";
-// const baseUrl = "https://localhost:7248/api/";
+const baseUrl = "https://aaaerp20-001-site1.jtempurl.com/api/";
+//const baseUrl = "https://localhost:7248/api/";
 const payableNotesId = "7c05577c-fbe1-46bd-8555-08dc3dfcb068";
 const receivableNotesId = "6066c2c6-4e74-4fc0-850e-08dc3dfcb068";
 
