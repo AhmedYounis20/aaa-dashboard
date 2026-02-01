3f99533a18de3c053b1066556c7d6d242e6e8b54
Mohamed shaheen <mohamedshaheen1898@gmail.com>
2026-02-02 00:15:04 +0200

    add server-side pagination
    Fix

--- src/Pages/ProjectPages/Account/AccountGuides/AccountGuidesRoot.tsx
+++ src/Pages/ProjectPages/Account/AccountGuides/AccountGuidesRoot.tsx
@@ -1,80 +1,77 @@
-import { useEffect, useState } from 'react';
+import { Box } from '@mui/material';
+import { useState } from 'react';
+import { useTranslation } from 'react-i18next';
+import { getAccountGuidesPaginated } from '../../../../Apis/Account/AccountGuidesApi';
 import { AppContent } from '../../../../Components';
 import { FormTypes } from '../../../../interfaces/Components';
 import AccountGuidesForm from './AccountGuidesForm';
-import { Box } from '@mui/material';
-import Loader from '../../../../Components/Loader';
-import { getAccountGuides } from "../../../../Apis/Account/AccountGuidesApi";
-import { AccountGuideModel } from '../../../../interfaces/ProjectInterfaces';
-import { useTranslation } from 'react-i18next';
 
 const AccountGuidesRoot = () => {
   const [showForm, setShowForm] = useState<boolean>(false);
   const [formType, setFormType] = useState<FormTypes>(FormTypes.Add);
   const [selectedId, setSelectedId] = useState<string>("");
-  const [data, setData] = useState<AccountGuideModel[]>([]);
-  const [isLoading, setIsLoading] = useState<boolean>(true);
+  const [reloadKey, setReloadKey] = useState(0);
   const {t} = useTranslation();
-  const fetchData = async () => {
-    const result = await getAccountGuides();
-    if (result && result.isSuccess) {
-      setData(result.result);
-      setIsLoading(false);
-    }
-  };
-  useEffect(() => {
-    fetchData();
-  }, []);
 
   const handleShowForm = () => {
     setShowForm(true);
   };
+
   const handleCloseForm = () => {
     setShowForm(false);
   };
   const handleSelectId: (id: string) => void = (id) => setSelectedId(id);
+
+  const columns: { Header: string; accessor: string }[] = [
+    {
+      Header: "Name",
+      accessor: "name",
+    },
+    {
+      Header: "NameSecondLanguage",
+      accessor: "nameSecondLanguage",
+    },
+  ];
+
   return (
     <div className="w-full">
-      {isLoading ? (
-        <Loader />
-      ) : (
-        <Box>
-          {showForm && (
-            <AccountGuidesForm
-              id={selectedId}
-              handleCloseForm={handleCloseForm}
-              formType={formType}
-              afterAction={() => fetchData()}
-              handleTranslate ={(key: string) => t(key)}
-            />
-          )}
+      <Box>
+        {showForm && (
+          <AccountGuidesForm
+            id={selectedId}
+            handleCloseForm={handleCloseForm}
+            formType={formType}
+            afterAction={() => setReloadKey(prev => prev + 1)}
+            handleTranslate ={(key: string) => t(key)}
+          />
+        )}
 
-          {!isLoading && (
-            <AppContent
-              tableType="table"
-              data={data}
-              title={t("AccountGuides")}
-              addBtn
-              btn
-              startIcon
-              actionBtn={() => {
-                setFormType(FormTypes.Add);
-                handleShowForm();
-              }}
-              handleSelectId={handleSelectId}
-              changeFormType={setFormType}
-              handleShowForm={handleShowForm}
-              defaultHiddenCols={[
-                "id",
-                "createdAt",
-                "createdBy",
-                "modifiedAt",
-                "modifiedBy",
-              ]}
-            />
-          )}
-        </Box>
-      )}
+        <AppContent
+          tableType="table"
+          title={t("AccountGuides")}
+          columns={columns}
+          addBtn
+          btn
+          startIcon
+          actionBtn={() => {
+            setFormType(FormTypes.Add);
+            handleShowForm();
+          }}
+          handleSelectId={handleSelectId}
+          changeFormType={setFormType}
+          handleShowForm={handleShowForm}
+          defaultHiddenCols={[
+            "id",
+            "createdAt",
+            "createdBy",
+            "modifiedAt",
+            "modifiedBy",
+          ]}
+          reloadKey={reloadKey}
+          serverSidePagination={true}
+          onFetchData={getAccountGuidesPaginated}
+        />
+      </Box>
     </div>
   );
 }
